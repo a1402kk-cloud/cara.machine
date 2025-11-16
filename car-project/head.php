dac5066ee2e1467e038c7d5739b7124b4aec5983
a1402kk-cloud <a.1402kk@gmail.com>
2025-11-16 13:23:41 +0330

    vamir2

---
+++ car-project/head.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav class="menu">
+    <a href="index.html"><button class="home">Home</button></a>
+    <a href="calculator.html"><button class="cal">Calculator</button></a>
+    <a href="about-of-us.html"><button class="about-us">About Us</button></a>
+    <a href="login.html"><button class="sign">login</button></a>
+  </nav>
+</body>
+</html>
---
+++ car-project/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include('head.php'); ?>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Caramachine</title>
+  <link rel="stylesheet" href="index.css" />
+</head>
+<body>
+
+  
+
+  <h1 class="h1">CARS</h1>
+
+  <div class="albums-photo">
+    <div class="photo-frame">
+      <a href="samand.html">
+        <img src="ceb4e14c0507033724b9896378188b4a15b177b0_1594213385.jpg" class="img" />
+      </a>
+    </div>
+
+    <div class="photo-frame">
+      <a href="#">
+        <img src="652cfa508716201626cef028.jpg" class="img" />
+      </a>
+    </div>
+
+    <div class="photo-frame">
+      <img src="638baf98e828122fa3242321.jpg" class="img" />
+    </div>
+  </div>
+</body>
+</html>
